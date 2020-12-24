5b930b14a9770d4486b72f0707c3f4ef13ee7221
MorfeoRenai <57602839+MorfeoRenai@users.noreply.github.com>
2020-12-24 19:00:31 +0100

    commented

--- sra-toolkit.sh
+++ sra-toolkit.sh
@@ -1,8 +1,9 @@
-VAR=$(cut -d ',' -f 1 SraRunTable.txt | tail -n +2)
+VAR=$(cut -d ',' -f 1 SraRunTable.txt | tail -n +2) # select the first field, containg the IDs needed for the sra-toolkit, and eliminate the header "Run"
 
 for i in ${VAR}
     do
         echo "Download SRA sample: ${i}"
-        prefetch ${i}
-        fastq-dump --gzip --defline-qual '+' ${i}
+        prefetch ${i}                               # for each run ID we prefetch the data...
+        fastq-dump --gzip --defline-qual '+' ${i}   # ...and then download it already decrompressed 
     done
+    
